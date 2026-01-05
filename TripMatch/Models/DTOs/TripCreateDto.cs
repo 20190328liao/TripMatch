dd8a1874634e9c9300d0e8a45c665bf0d1512d0d
zachchen1227 <zachchen1227@gmail.com>
2026-01-05 10:46:51 +0800

    更新建立行程

---
+++ TripMatch/Models/DTOs/TripCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TripMatch.Models.DTOs
+{
+    public class TripCreateDto
+    {
+        public string TripName { get; set; } = string.Empty;
+        public List<string> LocationName { get; set; } = [];
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
