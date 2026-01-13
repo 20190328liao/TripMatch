b5d227616a28fd0a07cdd1bba1edb937e6f07a3e
Yun Jen Huang <janehstza@gmail.com>
2026-01-13 11:24:29 +0800

    時間媒合前端頁面

--- TripMatch/Controllers/MatchController.cs
+++ TripMatch/Controllers/MatchController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TripMatch.Controllers
 {
     public class MatchController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
